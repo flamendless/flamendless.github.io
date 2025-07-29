47f629e22c018a00ebf1db06a9e64f05ed53d688
flamendless <flamendless@gmail.com>
2025-07-30 07:54:08 +0800

    Update deploy.sh to prompt pw instead

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,16 @@
 
 # Brandon Blanker Lim-it @flamendless
 
-SSH_ADDR="${SSH_ADDR}"
-PW="${PW}"
-
-if [ -z "${SSH_ADDR}" ]; then
-    echo "Provide SSH_ADDR"
-    exit 1
-fi
-
-if [ -z "${PW}" ]; then
-    echo "Provide PW"
-    exit 1
-fi
-
 set -euf -o pipefail
 
 echo "Building..."
 bundle exec jekyll build
 
-echo "Copying to server..."
-sshpass -f "${PW}" scp -v -r "./_site" "${SSH_ADDR}:/var/www/"
+SSH_ADDR="${SSH_ADDR}"
+if [ -z "${SSH_ADDR}" ]; then
+    echo "Provide SSH_ADDR"
+    exit 1
+fi
+
+read -s -p "Enter password: " pw
+sshpass -p "${pw}" scp -v -r "./_site" "${SSH_ADDR}:/var/www/"
